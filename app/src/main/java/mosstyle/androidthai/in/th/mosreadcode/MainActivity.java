c906900792cd5b563d80ef5bf8adfc8a5b34f5d0
master <mosza_style@hotmail.com>
2018-03-20 14:47:21 +0700

    Initial commit

---
+++ app/src/main/java/mosstyle/androidthai/in/th/mosreadcode/MainActivity.java
@@ -0,0 +1,13 @@
+package mosstyle.androidthai.in.th.mosreadcode;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
