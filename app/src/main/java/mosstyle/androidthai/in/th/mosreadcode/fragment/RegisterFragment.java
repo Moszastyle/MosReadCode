897ec64f0809fdcb5ef26de11e65fa1cdf0eeabb
master <mosza_style@hotmail.com>
2018-03-21 14:28:37 +0700

    Crete RegisterFragment Success

--- app/src/main/java/mosstyle/androidthai/in/th/mosreadcode/fragment/RegisterFragment.java
+++ app/src/main/java/mosstyle/androidthai/in/th/mosreadcode/fragment/RegisterFragment.java
@@ -1,20 +1,49 @@
 package mosstyle.androidthai.in.th.mosreadcode.fragment;
 
-import android.app.Fragment;
+
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import mosstyle.androidthai.in.th.mosreadcode.MainActivity;
 import mosstyle.androidthai.in.th.mosreadcode.R;
 
 /**
  * Created by User on 21/3/2561.
  */
 
-public class RegisterFragment extends Fragment{
+public class RegisterFragment extends Fragment  {
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        Create Toolbar
+
+        Toolbar toolbar = getView().findViewById(R.id.toolbarRegister);
+        ((MainActivity)getActivity()).setSupportActionBar(toolbar);
+
+//        Setup Title
+        ((MainActivity) getActivity()).getSupportActionBar().setTitle("Register");
+
+//        Show Navigator Icon
+
+        ((MainActivity) getActivity()).getSupportActionBar().setHomeButtonEnabled(true);
+        ((MainActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getActivity().getSupportFragmentManager().popBackStack();
+            }
+        });
+
+
+    }   //Main Method
 
     @Nullable
     @Override
