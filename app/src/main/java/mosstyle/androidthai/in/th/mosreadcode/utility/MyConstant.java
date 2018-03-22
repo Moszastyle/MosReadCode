0797da886ff6c3630ec91e57deab4d5b37723a9f
master <mosza_style@hotmail.com>
2018-03-22 15:52:15 +0700

    Authen ok

--- app/src/main/java/mosstyle/androidthai/in/th/mosreadcode/utility/MyConstant.java
+++ app/src/main/java/mosstyle/androidthai/in/th/mosreadcode/utility/MyConstant.java
@@ -8,8 +8,22 @@ public class MyConstant {
 
     //    About URL
     private String urlPostUserString = "http://androidthai.in.th/mar/postUser.php";
+    private String urlGetAllUserString = "http://androidthai.in.th/mar/getAllUser.php";
+
+    // About Array
+    private String[] loginStrings = new String[]{"id","Name","User","Password"};
+
+    public String[] getLoginStrings() {
+        return loginStrings;
+    }
+
+    public String getUrlGetAllUserString() {
+        return urlGetAllUserString;
+    }
 
     public String getUrlPostUserString() {
         return urlPostUserString;
+
+
     }
 } //main class
